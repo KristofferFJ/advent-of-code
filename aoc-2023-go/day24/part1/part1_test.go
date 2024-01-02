f621e055139c82969e23b8f5c2547aa04b69251a
kfj <kristofferfage@gmail.com>
2024-01-02 17:59:35 +0100

    day 24 part1 cleaned up slightly

--- aoc-2023-go/day24/part1/part1_test.go
+++ aoc-2023-go/day24/part1/part1_test.go
@@ -2,7 +2,6 @@ package part1
 
 import (
 	"fmt"
-	"github.com/shopspring/decimal"
 	"io.kristofferfj.github/aoc-2023-go/util"
 	"strings"
 	"testing"
@@ -14,17 +13,17 @@ var InputTest = `19, 13, 30 @ -2,  1, -2
 12, 31, 28 @ -1, -2, -1
 20, 19, 15 @  1, -5, -3`
 
-type Point struct{ x, y, z decimal.Decimal }
+type Point struct{ x, y, z float64 }
 type Hail struct{ pos, vel Point }
-type Trajectory struct{ a, b decimal.Decimal }
+type Trajectory struct{ a, b float64 }
 
-var zero = decimal.NewFromInt(int64(0))
+var zero = float64(0)
 
 func TestInput(t *testing.T) {
 	hails := parseInput(Input)
 
-	minVal := decimal.NewFromInt(int64(200000000000000))
-	maxVal := decimal.NewFromInt(int64(400000000000000))
+	minVal := float64(200000000000000)
+	maxVal := float64(400000000000000)
 
 	count := 0
 	for i := 0; i < len(hails)-1; i++ {
@@ -32,12 +31,12 @@ func TestInput(t *testing.T) {
 			hail1 := hails[i]
 			hail2 := hails[j]
 			x, y, t1, t2 := getCrossing(hail1, hail2)
-			if x.GreaterThanOrEqual(minVal) &&
-				x.LessThanOrEqual(maxVal) &&
-				y.GreaterThanOrEqual(minVal) &&
-				y.LessThanOrEqual(maxVal) &&
-				t1.GreaterThan(zero) &&
-				t2.GreaterThan(zero) {
+			if x >= minVal &&
+				x <= maxVal &&
+				y >= minVal &&
+				y <= maxVal &&
+				t1 > zero &&
+				t2 > zero {
 				count++
 			}
 		}
@@ -48,21 +47,21 @@ func TestInput(t *testing.T) {
 
 func getTrajectory(h Hail) Trajectory {
 	return Trajectory{
-		a: h.vel.y.Div(h.vel.x),
-		b: (h.vel.y.Neg().Mul(h.pos.x.Div(h.vel.x))).Add(h.pos.y),
+		a: h.vel.y / (h.vel.x),
+		b: -h.vel.y*(h.pos.x/(h.vel.x)) + h.pos.y,
 	}
 }
 
-func getCrossing(h1, h2 Hail) (decimal.Decimal, decimal.Decimal, decimal.Decimal, decimal.Decimal) {
+func getCrossing(h1, h2 Hail) (float64, float64, float64, float64) {
 	trajectory1 := getTrajectory(h1)
 	trajectory2 := getTrajectory(h2)
-	if trajectory1.a.Equals(trajectory2.a) {
-		return decimal.Decimal{}, decimal.Decimal{}, decimal.Decimal{}, decimal.Decimal{}
+	if trajectory1.a == trajectory2.a {
+		return 0, 0, 0, 0
 	}
-	x := (trajectory2.b.Sub(trajectory1.b)).Div(trajectory1.a.Sub(trajectory2.a))
-	y := (trajectory1.a.Mul(x)).Add(trajectory1.b)
-	t1 := (x.Sub(h1.pos.x)).Div(h1.vel.x)
-	t2 := (x.Sub(h2.pos.x)).Div(h2.vel.x)
+	x := (trajectory2.b - trajectory1.b) / (trajectory1.a - trajectory2.a)
+	y := (trajectory1.a * x) + trajectory1.b
+	t1 := (x - h1.pos.x) / h1.vel.x
+	t2 := (x - h2.pos.x) / h2.vel.x
 	return x, y, t1, t2
 }
 
@@ -71,8 +70,8 @@ func parseInput(input string) (hails []Hail) {
 		split := util.IntArray(line)
 		hails = append(hails,
 			Hail{
-				pos: Point{decimal.NewFromInt(int64(split[0])), decimal.NewFromInt(int64(split[1])), decimal.NewFromInt(int64(split[2]))},
-				vel: Point{decimal.NewFromInt(int64(split[3])), decimal.NewFromInt(int64(split[4])), decimal.NewFromInt(int64(split[5]))},
+				pos: Point{float64(split[0]), float64(split[1]), float64(split[2])},
+				vel: Point{float64(split[3]), float64(split[4]), float64(split[5])},
 			})
 	}
 
